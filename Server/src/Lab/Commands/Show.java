0df11a5d3c482d3c904a6de021eb927788b137c9
SuperJaremy <47501002+SuperJaremy@users.noreply.github.com>
2020-06-16 15:52:48 +0500

    Версия 1.0001
    Добавлена сортировка по размеру

--- Server/src/Lab/Commands/Show.java
+++ Server/src/Lab/Commands/Show.java
@@ -4,6 +4,9 @@ import Lab.Objects.MusicBand;
 import Lab.Service.Answer;
 import Lab.Service.Work;
 
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class Show  extends Command{
     {
         name="show";
@@ -16,11 +19,9 @@ public class Show  extends Command{
 
     @Override
     public Answer act(Meta meta, Work work) {
-        String info="";
-        for(MusicBand i : work.getV()){
-            info=info.concat(i.toString()).concat("\n");
-        }
-        info=info.concat("Конец списка");
+        String info= work.getV().stream().sorted(Comparator.comparing(MusicBand::getSize)).
+                map(MusicBand::toString).map(mb->mb.concat("\n")).
+                collect(Collectors.joining()).concat("Конец списка");
         return new Answer(info,true,exit);
     }
 }
